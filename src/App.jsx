a584f73a468dd53095a2e744f29d2f8bf4015dfc
Kavitha Kanchana <kanchanakavitha6@gmail.com>
2025-07-04 09:52:53 +0530

    Add About, Art, and Menu components with GSAP animations and responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
+import Art from "./components/Art";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -12,6 +15,9 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
+      <Art />
+      <Menu />
     </main>
   );
 };
